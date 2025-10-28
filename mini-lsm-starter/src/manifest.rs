09142decae690c64eb81b5d33351f055d2eafdb5
Xingyu Long <xingyulong97@gmail.com>
2025-10-28 14:10:25 -0700

    week2_day5: task1, encode/decode for Manifest

--- mini-lsm-starter/src/manifest.rs
+++ mini-lsm-starter/src/manifest.rs
@@ -15,13 +15,16 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Read;
 use std::path::Path;
 use std::sync::Arc;
+use std::{fs::File, io::Write};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use parking_lot::{Mutex, MutexGuard};
 use serde::{Deserialize, Serialize};
+use serde_json::Deserializer;
 
 use crate::compact::CompactionTask;
 
@@ -38,11 +41,40 @@ pub enum ManifestRecord {
 
 impl Manifest {
     pub fn create(_path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+        Ok(Self {
+            file: Arc::new(Mutex::new(
+                OpenOptions::new()
+                    .read(true)
+                    .create_new(true)
+                    .write(true)
+                    .open(_path)
+                    .context("failed to create Manifest file")?,
+            )),
+        })
     }
 
     pub fn recover(_path: impl AsRef<Path>) -> Result<(Self, Vec<ManifestRecord>)> {
-        unimplemented!()
+        let mut file = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .open(_path)
+            .context("failed to recover Manifest file")?;
+
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+
+        let mut stream = Deserializer::from_slice(&buf).into_iter::<ManifestRecord>();
+        let mut records = Vec::new();
+        while let Some(record) = stream.next() {
+            records.push(record?);
+        }
+
+        Ok((
+            Self {
+                file: Arc::new(Mutex::new(file)),
+            },
+            records,
+        ))
     }
 
     pub fn add_record(
@@ -54,6 +86,14 @@ impl Manifest {
     }
 
     pub fn add_record_when_init(&self, _record: ManifestRecord) -> Result<()> {
-        unimplemented!()
+        // what's the format of Manifest?
+        let encoded = serde_json::to_vec(&_record)?;
+        {
+            let mut file = self.file.lock();
+            file.write(&encoded)?;
+            file.sync_all()?;
+        }
+
+        Ok(())
     }
 }
